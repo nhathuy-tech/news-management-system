510d02f0a66f19101cdf23affca5ba3fb38beb01
huyphan67 <huypdn6703@gmail.com>
2025-06-08 14:42:49 +0700

    chore(setup): initialize Spring Boot project with Maven

---
+++ src/main/java/com/newsportal/news_management_system/NewsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.newsportal.news_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newsportal/news_management_system/NewsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsportal.news_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
